68bd87bb6d12b8e512b366aec2e6773397a30b8f
Crowdbotics <team@crowdbotics.com>
2021-08-03 02:34:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "late_flower_29366Identifier",
+  url: "https://late_flower_29366Identifier.botics.co",
+  api: "https://late_flower_29366Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
